a64867ab60645d6495c359a27a44458d114874b8
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2022-04-13 11:51:50 +0200

    Filtering added to unpacking.

--- electron/utilities.py
+++ electron/utilities.py
@@ -9,13 +9,20 @@ def load_data(filename : str):
     return dataframe.AsNumpy(), dataframe.GetColumnNames()
 
 
-def unpack_digit_indices(digit_indices):
+def unpack_digit_indices(digit_indices, filter:bool=False):
     unpacked_digit_indices = []
     for index in range(6):
         column = np.ndarray(len(digit_indices), dtype=int)
         for column_index in range(len(digit_indices)):
             column[column_index] = digit_indices[column_index][index]
         unpacked_digit_indices.append(column)
+    if filter:
+        to_filter = []
+        for index in range(len(unpacked_digit_indices)):
+            if len(np.unique(unpacked_digit_indices[index])) == 1:
+                print(f"Column {index} is being filtered out.")
+                to_filter.append(index)
+        unpacked_digit_indices = np.delete(unpacked_digit_indices, to_filter, axis=0)
     return unpacked_digit_indices
 
 
