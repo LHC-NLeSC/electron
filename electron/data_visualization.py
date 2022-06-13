0acc2addd93dd04a604b8887e945a8ea48ea007f
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2022-06-13 14:23:49 +0200

    Tweaking visualization.

--- electron/data_visualization.py
+++ electron/data_visualization.py
@@ -22,30 +22,37 @@ def __main__():
     if "calo_energy" not in columns:
         print("Missing data.")
         return
-    # plot energy for all tracks and only electrons
+    # plot energy for all tracks
     data = dataframe["calo_energy"]
     plt.plot(data, "o")
     plt.show()
-    # plot energy for just electrons
-    data = dataframe["calo_energy"]
+    # plot energy for electrons
     data_electrons = data[labels == 1]
     plt.plot(data_electrons, "o")
     plt.show()
-    # plot E/p for all data
+    # plot histogram of energy for all tracks
+    # bins = [100 * i for i in range(0, 210)]
+    plt.hist(data, bins=1000, histtype="step")
+    plt.xlabel("Energy")
+    plt.ylabel("Tracks")
+    plt.show()
+    # plot histogram of energy for electrons
+    plt.hist(data_electrons, bins=1000, histtype="step")
+    plt.xlabel("Energy")
+    plt.ylabel("Tracks")
+    plt.show()
+    # plot E/p for all tracks
     if "ep" not in columns:
         print("Missing data.")
         return
     data = dataframe["ep"]
-    bins = [0.01 * i for i in range(0, 210)]
-    plt.hist(data, bins=bins, histtype="step")
+    plt.hist(data, bins=100, histtype="step")
     plt.xlabel("E/p")
     plt.ylabel("Tracks")
     plt.show()
     # # plot E/p for electrons
-    data = dataframe["ep"]
     data_electrons = data[labels == 1]
-    bins = [0.01 * i for i in range(0, 210)]
-    plt.hist(data_electrons, bins=bins, histtype="step")
+    plt.hist(data_electrons, bins=100, histtype="step")
     plt.xlabel("E/p")
     plt.ylabel("Tracks")
     plt.show()
