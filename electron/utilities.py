9e8131760260a8f76943d89959ac247a175d3a37
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2022-08-22 12:26:16 +0200

    Compute accuracy with the threshold method.

--- electron/utilities.py
+++ electron/utilities.py
@@ -3,7 +3,6 @@ from ROOT import TFile, RDataFrame
 
 
 def load_data(filename : str):
-    global columns
     kalman_file = TFile(filename)
     dataframe = RDataFrame("kalman_validator/kalman_ip_tree", kalman_file)
     dataframe = dataframe.Define("p", "abs(1.f/best_qop)")
@@ -15,3 +14,23 @@ def shuffle_data(rng, data, labels):
     assert(len(data) == len(labels))
     permutation = rng.permutation(len(data))
     return data[permutation], labels[permutation]
+
+
+def threshold_method(data, labels, threshold=0.7):
+    true_positives = 0
+    true_negatives = 0
+    false_positives = 0
+    false_negatives = 0
+    for index in range(0, len(data)):
+        positive = False
+        if data[index][6] > threshold:
+            positive = True
+        if positive and labels[index][0] == 1:
+            true_positives = true_positives + 1
+        elif not positive and labels[index][0] == 0:
+            true_negatives = true_negatives + 1
+        elif positive and labels[index][0] == 0:
+            false_positives = false_positives + 1
+        elif not positive and labels[index][0] == 1:
+            false_negatives = false_negatives + 1
+    return (true_positives + true_negatives) / (true_positives + true_negatives + false_positives + false_negatives)
