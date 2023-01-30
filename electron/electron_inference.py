af93bacbd09de6edcbc0cfd15143fe2b5a0e09da
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2023-01-30 16:22:30 +0100

    Use correct data.

--- electron/electron_inference.py
+++ electron/electron_inference.py
@@ -11,6 +11,10 @@ from utilities import load_data
 # reduce TensorFlow verbosity
 environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
+model_columns = ["eop", "best_pt", "best_qop", "chi2", "chi2V", "first_qop", "ndof", "ndofT",
+    "ndofV", "p", "qop", "tx", "ty", "x", "y", "z", "n_vertices", "n_tracks", "kalman_ip_chi2", "ecal_energy", 
+    "ecal_digit_0", "ecal_digit_1", "ecal_digit_2", "ecal_digit_3", "ecal_digit_4", "ecal_digit_5"]
+
 
 def command_line():
     parser = argparse.ArgumentParser()
@@ -21,8 +25,12 @@ def command_line():
 
 def __main__():
     arguments = command_line()
-    dataframe, _ = load_data(arguments.filename)
-    data = [dataframe[i] for i in range(0, len(dataframe))]
+    dataframe, columns = load_data(arguments.filename)
+    for column in model_columns:
+        if column not in columns:
+            print("Missing data.")
+            return
+    data = [dataframe[column] for column in model_columns]
     # read model
     model = tf.keras.models.load_model(arguments.model)
     # inference
